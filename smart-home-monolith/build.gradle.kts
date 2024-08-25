cebbba5914cb933c5bad07504760b6fd74b28082
Алексей Жмылев <azhmylev@seven.tech>
2024-08-26 01:49:17 +0300

    monolith fix

---
+++ smart-home-monolith/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.9"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "ru.yandex.practicum"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.kafka:spring-kafka")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("org.testcontainers:junit-jupiter")
+
+    runtimeOnly("org.postgresql:postgresql")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
--- smart-home-monolith/src/main/java/ru/yandex/practicum/smarthome/config/KafkaConfig.java
+++ smart-home-monolith/src/main/java/ru/yandex/practicum/smarthome/config/KafkaConfig.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.smarthome.config;
 
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.ssl.SslBundles;
@@ -15,6 +16,7 @@ import org.springframework.kafka.support.serializer.JsonDeserializer;
 import ru.yandex.practicum.smarthome.in.messaging.DeviceEvent;
 
 @Configuration
+@ConditionalOnBean(KafkaProperties.class)
 public class KafkaConfig {
 
     @Bean
